4071cea74d75d7e17ac3720a0c51cba52c48199a
minikozort <gokselgokkocabas@gmail.com>
2024-09-25 22:10:20 -0400

    added functionality for singlerecipe file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,40 @@
+// const API_KEY2 = process.env.REACT_APP_API_KEY2;
+
 export const searchSpoonacular = (query) => {
-    return fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=1f6a58783ebd4dbe8643444012dfec59`);
+    return fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=e675da75cbd340718699bc2539228d08`);
 };
+
+export const searchSpoonacularById = async (id) => {
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/${id}/information?apiKey=e675da75cbd340718699bc2539228d08`
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to fetch recipe information");
+      }
+  
+      const data = await response.json(); // Extract JSON data
+      return data; // Return the parsed data
+    } catch (error) {
+      console.error("Error fetching recipe:", error);
+    }
+  };
+
+  export const searchSpoonacularInstructions = async (id) => {
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=e675da75cbd340718699bc2539228d08`
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to fetch recipe instructions");
+      }
+  
+      const data = await response.json(); // Extract JSON data
+      return data; // Return the parsed data
+    } catch (error) {
+      console.error("Error fetching instructions:", error);
+    }
+  };
+  
