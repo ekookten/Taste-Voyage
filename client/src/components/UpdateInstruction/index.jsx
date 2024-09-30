dde2848512ee3b6c5de093fc9eaf40b8dd3df5e4
erkanoktenn <erkannoktenn@gmail.com>
2024-09-29 22:05:34 -0400

    created UpdateInstruction folder and finished the needed codes

---
+++ client/src/components/UpdateInstruction/index.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { UPDATE_INSTRUCTION } from "../../utils/mutations";
+import { useMutation } from "@apollo/client";
+
+function UpdateInstruction({
+  instruction,
+  instructions,
+  setInstructions,
+  index,
+}) {
+  const [updateInstruction] = useMutation(UPDATE_INSTRUCTION);
+
+  const [newInstruction, setNewInstruction] = useState(instruction.text);
+  const [showForm, setShowForm] = useState(false);
+
+  const handleUpdateInstruction = async (e) => {
+    e.preventDefault();
+    try {
+      const stepNumber = index + 1;
+      await updateInstruction({
+        variables: {
+          instructionId: instruction._id,
+          text: newInstruction.trim(),
+          step: stepNumber.toString(),
+        },
+      });
+
+      const updatedInstructions = instructions.map((i) => {
+        if (i._id === instruction._id) {
+          i.text = newInstruction;
+        }
+        return i;
+      });
+      setInstructions(updatedInstructions);
+      setShowForm(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const handleShowInstructionInput = () => {
+    setShowForm(true);
+  };
+
+  return (
+    <>
+      {showForm ? (
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <input
+              className="input"
+              type="text"
+              value={newInstruction}
+              onChange={(e) => setNewInstruction(e.target.value)}
+              placeholder="New Instruction"
+            />
+          </div>
+          <div className="control">
+            <button
+              type="button"
+              className="button is-link"
+              onClick={handleUpdateInstruction}
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <p>
+            Step {index + 1}: {instruction.text}
+          </p>
+          <button
+            type="button"
+            className="button is-link"
+            onClick={handleShowInstructionInput}
+          >
+            Edit
+          </button>
+        </>
+      )}
+    </>
+  );
+}
+
+export default UpdateInstruction;
